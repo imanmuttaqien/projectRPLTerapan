d6180cbbf5028976db4bfe839267b272c97ed508
aetthea71 <aetthea@gmail.com>
2014-12-17 08:19:28 +0700

    Master commit

---
+++ test/app/Model/user.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class user {
+	
+	int x;
+	int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+	
+}
---
+++ test/app/controllers/Application.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.*;
+import play.data.validation.Required;
+import play.mvc.*;
+import java.util.*;
+import models.*;
+
+public class Application extends Controller {
+
+	public static void index() {
+	    render();
+	}
+// Test
+	public static void sayHello(@Required String myName) {
+		if (validation.hasErrors()){
+	   		flash.error("Heit, field nama tidak boleh kosong");
+	  		index();
+	   	}
+		render(myName);
+	}
+}
