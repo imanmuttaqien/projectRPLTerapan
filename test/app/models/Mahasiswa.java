454c6e42a94456deb0f072478047c11b0d4f6810
imanmuttaqien <iman.muttaqien22@gmail.com>
2015-01-27 11:10:11 -0800

    Update Mahasiswa.java

--- test/app/models/Mahasiswa.java
+++ test/app/models/Mahasiswa.java
@@ -17,7 +17,7 @@ public class Mahasiswa extends Model {
 	public String alamat;
 	//@Required
 	public String pendidikan;
-	public Date tgllhr;
+	public string tgllhr;
     public String kodepos;
     @ManyToOne
     public JenisKelamin kelamin;
@@ -26,7 +26,27 @@ public class Mahasiswa extends Model {
       this.nama=nama;
     }
 	public String toString() {
-	      return nama;
+	      return this.nama;
 	   }
+	   public void setJenisKelamin(String kelamin){
+    		this.kelamin=kelamin;
+     }
+     	public String toString() {
+     		return this.kelamin;
+     	}
+	public void settgllhr(String tgllhr){
+		this.tgllhr=tgllhr;
+    }
+        public String toString() {
+        	return this.tgllhr;
+        }
+        public void setKodepos(String kodepos){
+        	this.kodepos=kodepos;
+    }
+	public String toString() {
+		return this.kodepos;
+	}
+	
+}
 	
 }
